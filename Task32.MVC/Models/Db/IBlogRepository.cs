16f184e8efe2dfa5c0835eb101eb268dcc80d4ba
Ekrador <wowezlo@gmail.com>
2023-07-18 01:36:36 +0500

    repository fixes

--- Task32.MVC/Models/Db/IBlogRepository.cs
+++ Task32.MVC/Models/Db/IBlogRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace Task32.MVC.Models.Db
+﻿using System.Threading.Tasks;
+
+namespace Task32.MVC.Models.Db
 {
     public interface IBlogRepository
     {
+        Task AddUser(User user);
     }
 }
